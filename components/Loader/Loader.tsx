29e5c0fb4297794dd87b1b49dbb59a0d376cb6b4
Yaroslav <yaroslavlit@gmail.com>
2025-10-05 20:21:59 +0200

    add styles for IOS Safari

--- components/Loader/Loader.tsx
+++ components/Loader/Loader.tsx
@@ -1,5 +1,17 @@
+"use client";
+
+import { useEffect } from "react";
 import css from "./Loader.module.css";
+
 export default function Loader() {
+  useEffect(() => {
+    document.body.classList.add("no-scroll");
+
+    return () => {
+      document.body.classList.remove("no-scroll");
+    };
+  }, []);
+
   return (
     <div className={css.wrapper}>
       <span className={css.loader}></span>
